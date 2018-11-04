2ca9804c36673a70f50cab8ecd6f403f4b43d9b1
Tie C <usbtyping@gmail.com>
2018-11-04 10:50:58 -0800

    AutoBox Add Commit

---
+++ TeamCode/src/AutoBox.java
@@ -0,0 +1,221 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
+// nsfiojaspofji vijsgpioadg iodfj;as
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Drew Drive 3", group="Linear Opmode")
+//@Disabled
+public class DrewDrive3 extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightBackDrive = null;
+    private DcMotor leftFrontDrive = null;
+    private DcMotor rightFrontDrive = null;
+    //marker arm
+    private Servo markerArm = null;
+    //lift and hinge
+    private DcMotor hinge = null;
+    private DcMotor lift = null;
+    //mineral stuff
+    private DcMotor arm = null;
+    private Servo leftHand = null;
+    private Servo rightHand = null;
+
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rightBack");
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFront");
+        lift = hardwareMap.get(DcMotor.class, "lift");
+        markerArm = hardwareMap.get(Servo.class,"markerServo");
+        hinge = hardwareMap.get(DcMotor.class, "hinge");
+        leftHand = hardwareMap.get(Servo.class, "leftHand");
+        rightHand = hardwareMap.get(Servo.class, "rightHand");
+        arm = hardwareMap.get(DcMotor.class, "arm");
+         
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        lift.setDirection(DcMotor.Direction.FORWARD);
+        hinge.setDirection(DcMotor.Direction.FORWARD);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        //mode id, 0 = normal sped, 1 = speedivided b dy the slow modifier
+        int mode = 0;
+        int slow = 4;
+
+        //open or closed state for the mineral arm
+        int handState = 0;
+
+        //leftHand.setPosition(1);
+        //rightHand.setPosition(0.5);
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double leftPower;
+            double rightPower;
+
+            // Tank Mode uses one stick to control each wheel.
+            // - This requires no math, but it is hard to drive forward slowly and keep straight.
+            leftPower  = gamepad1.left_stick_y;
+            rightPower = -gamepad1.right_stick_y;
+            
+            
+            //mode change - 1 = normal speed, 0 = slow mode
+            if(gamepad1.a) {
+                if(mode == 0){
+                    mode = 1;
+                } else {
+                    mode = 0;
+                }
+            }
+
+            // Send calculated power to wheels
+            if(mode == 0){
+                leftPower  = gamepad1.left_stick_y;
+                rightPower = gamepad1.right_stick_y;
+            } else {
+                leftPower  = gamepad1.left_stick_y/slow;
+                rightPower = gamepad1.right_stick_y/slow;
+            }
+            
+            //move the lift
+            if(gamepad2.dpad_down) {
+                lift.setPower(.75);
+            }
+            else if(gamepad2.dpad_up) {
+                lift.setPower(-.75);
+            }
+            else {
+                lift.setPower(0);
+            }
+
+            //move the hook thats on the lift
+            if(gamepad2.dpad_left) {
+                hinge.setPower(-0.125);
+            }
+            else if(gamepad2.dpad_right) {
+                hinge.setPower(0.125);
+            }
+            else {
+                hinge.setPower(0);
+            }
+            
+            // leftHand.setPosition(0.2 - gamepad2.left_trigger);
+            // rightHand.setPosition(gamepad2.right_trigger + 0.2);
+            
+            //open and close the hand to grab minerals based on hand state
+            //DONT CHANGE THIS CHEF!!
+            if(gamepad2.x){
+                if(handState == 0) {
+                    handState = 1;
+                    telemetry.addData("Hand state:", "s", handState);
+                } else if(!gamepad2.x) {
+                    handState = 0;
+                    telemetry.addData("Hand state:", "s", handState);
+                }
+            }
+            
+            if(handState == 0){
+                //closed
+                leftHand.setPosition(.0);
+                rightHand.setPosition(1);
+            } else {
+                //open
+                leftHand.setPosition(.25);
+                rightHand.setPosition(.5);
+            }
+            
+            //lift and lower mineral arm
+            if(gamepad2.right_bumper){
+                arm.setPower(0.33);
+            } else if (gamepad2.left_bumper){
+                arm.setPower(-.4);
+            } else {
+                arm.setPower(0);
+            }
+            
+            //test stuff for pre-auto
+            if(!(gamepad2.right_trigger == 0)){
+                markerArm.setPosition(1);
+            } else {
+                markerArm.setPosition(0);
+            }
+
+            //move the robot with dat calculated powah
+            leftBackDrive.setPower(leftPower);
+            leftFrontDrive.setPower(leftPower);
+            rightBackDrive.setPower(rightPower);
+            rightFrontDrive.setPower(rightPower);
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.update();
+        }
+    }
+}
